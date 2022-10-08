f09af8e21d3330dcd0d8067a946469b2f871cb04
CrownKingPown <liongate3396@gmail.com>
2022-10-08 16:16:59 -0300

    eee

--- app.js
+++ app.js
@@ -22,31 +22,16 @@ $(document).ready(function() {
     return true;
   }
 
-  function hexToRgb(hex) {
-    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-      return r + r + g + g + b + b;
-    });
 
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-    } : null;
-  }
-
-  var img = new Image(1080, 1080);
   var lastBuffer;
-  document.body.appendChild(img);
+  var img = document.getElementById("streamImage");
 
   // message
   setInterval(async function() {
     var bufferImage = await getPixelData();
     if (bufferImage.byteLength != 0 && lastBuffer != bufferImage) {
       lastBuffer = bufferImage;
-      var blob = new Blob([bufferImage], { type: "image/png" });
+      var blob = new Blob([bufferImage], { type: "image/jpeg" });
 
       var domURL = self.URL || self.webkitURL || self,
         url = domURL.createObjectURL(blob)
@@ -56,5 +41,5 @@ $(document).ready(function() {
       };
       img.src = url;
     }
-  }, /*Delay in milliseconds*/ 200);
+  }, /*Delay in milliseconds*/ 150);
 });
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var currentImageData;
 const path = require("path");
+var compressjs = require('compressjs');
+var algorithmCompress = compressjs.Lzjb;
 const fileUpload = require("express-fileupload");
 const express = require('express')
 const app = express()
@@ -12,10 +14,9 @@ app.use(express.static(__dirname));
 
 app.post('/poststreamdata', (req, res) => {
   var file = req.files[Object.keys(req.files)[0]];
-  var bufferResult = file.data;
+  var bufferResult = new Buffer.from(algorithmCompress.decompressFile(file.data));
   if (bufferResult != null) {
     currentImageData = bufferResult;
-    res.send("Success");
   }
   else {
     res.send("Error!");
